f7e71bd1fa650caef2b792d78913f0f69c683def
Marcos <markitosanches@gmail.com>
2025-07-11 14:54:15 -0400

    séance 17

--- seance17/form.php
+++ seance17/form.php
@@ -4,12 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        input, select{
+            display: block;
+            margin: 5px;
+        }
+    </style>
 </head>
 <body>
-    
+    <h2>Form</h2>
+    <form action="submit-form.php" method="get">
+        <label for="name">Name</label>
+        <input id="name" type="text" name="name">
+        <label for="pwd">Password</label>
+        <input id="pwd" type="password" name="password">
+        <label for="age">Age</label>
+        <input id="age" type="number"  name="age">
+        <label for="city">City</label>
+        <select id="city"  name="city">
+            <option value="1">Montreal</option>
+            <option value="2">Quebec</option>
+            <option value="3">Laval</option>
+        </select>
+        <input type="submit">
+    </form>
 </body>
 </html>
 
-name
-age
-ville (Montreal, quebec, laval))
---
+++ seance17/submit-form.php
@@ -0,0 +1,10 @@
+<?php
+ print_r($_GET);
+// $_GET;
+// echo "<pre>";
+// var_dump($_POST);
+// echo "</pre>";
+
+
+echo "Salut ".$_GET['name'];
+?>
